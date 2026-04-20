cb38b8b41eb8e8a316ed50989a2b1080f47167ab
Qadeer  Ahmad <abdul-qadeer92>
2026-04-20 23:55:15 +0500

    global

---
+++ main.js
@@ -0,0 +1,16 @@
+
+let schoolName = "Al Noor School";
+
+function studentInfo() {
+   
+    let studentName = "Abdul Qadeer";
+
+    console.log("School Name: " + schoolName);
+    console.log("Student Name: " + studentName);
+}
+
+studentInfo();
+
+
+console.log("School Name Outside Function: " + schoolName);
+
